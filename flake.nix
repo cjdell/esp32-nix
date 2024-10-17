dc3844019e126ecd1bf3ee1007faeabb108c522b
Chris Dell <cjdell@gmail.com>
2024-10-17 16:35:02 +0100

    Downgrade to ESP-IDF 5.1

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs-esp-dev = {
-      url = "github:mirrexagon/nixpkgs-esp-dev";
+      url = "github:mirrexagon/nixpkgs-esp-dev/86a2bbe01fe0258887de7396af2a5eb0e37ac3be";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
@@ -25,7 +25,14 @@
             name = "esp32-nix";
 
             buildInputs = with pkgs; [
-              esp-idf-full
+              # esp-idf-full
+              (esp-idf-esp32.override {
+                rev = "632e0c2a9fc7c754db4135dabb67f7fc6aa9fb87";
+                sha256 = "sha256-j3NaEzMPg2qJ37Gy1y1rdaFdR1/047jOef0Sw/XnCzc=";
+                toolsToInclude = [
+                  "xtensa-esp32-elf"
+                ];
+              })
             ];
 
             ESP_DEV_PATH = nixpkgs-esp-dev;
